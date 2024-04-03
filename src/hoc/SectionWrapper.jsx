dc239f1b438afdb24645330cd52d89f76310ef2c
KrlosDev <carlosb@bbhint.com>
2024-04-02 22:06:46 -0500

    more responsiveness

--- src/hoc/SectionWrapper.jsx
+++ src/hoc/SectionWrapper.jsx
@@ -1,23 +1,40 @@
-import {motion} from 'framer-motion'
 import {styles} from '../styles'
 import { staggerContainer } from '../utils/motion'
 
+const isXsMobile = () => {
+  const width = window.innerWidth;
+  return width <= 375;
+}
+
 
 const SectionWrapper = (Component, idName) => function HOC(){
-  return(
-    <motion.section
-     variants={staggerContainer()}
-     initial="hidden"
-     whileInView="show"
-     viewport={{once:true, amount:0.25}}
-     className={`${styles.padding} max-w-7xl mx-auto relative z-0`}
-    >
-        <span className='hash-span' id={idName}>
-            &nbsp;
-        </span>
-        <Component/>
-    </motion.section>
-  )
+  if (isXsMobile) {
+    return(
+      <section
+       className={`${styles.padding} max-w-7xl mx-auto relative z-0`}
+      >
+          <span className='hash-span' id={idName}>
+              &nbsp;
+          </span>
+          <Component/>
+      </section>
+    )
+  } else {
+    return(
+      <motion.section
+       variants={staggerContainer()}
+       initial="hidden"
+       whileInView="show"
+       viewport={{once:true, amount:0.25}}
+       className={`${styles.padding} max-w-7xl mx-auto relative z-0`}
+      >
+          <span className='hash-span' id={idName}>
+              &nbsp;
+          </span>
+          <Component/>
+      </motion.section>
+    )
+  }
 }
 
 export default SectionWrapper
